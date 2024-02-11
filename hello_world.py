0f31101acafe47ef3ac268dfb2bb3ce99753ab69
shmar <sh.mariette@gmail.com>
2024-02-11 15:57:20 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 print("Hello World!")
-=======
-print("Hello world!")
->>>>>>> 9bb7a505374c63502be00fe0f9bbfff438b6a484
+
